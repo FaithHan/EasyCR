f8ec8e51d77260f955754075fb2635172a589481
feihan <feihan@thoughtworks.com>
2021-06-25 10:10:07 +0800

    Compatible with Pre-2020.3 version

--- src/main/java/com/easycr/notify/RecordNotifier.java
+++ src/main/java/com/easycr/notify/RecordNotifier.java
@@ -1,17 +1,19 @@
 package com.easycr.notify;
 
-import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nullable;
 
 public class RecordNotifier {
 
+
+    private static final NotificationGroup NOTIFICATION_GROUP =
+            new NotificationGroup("Custom Notification Group", NotificationDisplayType.BALLOON, true);
+
     public static void notifyInfo(@Nullable Project project, String content) {
-        NotificationGroupManager.getInstance().getNotificationGroup("com.easycr")
-                .createNotification(content, NotificationType.INFORMATION)
-                .setTitle("EasyCR add a new Item Success")
-                .notify(project);
+        NOTIFICATION_GROUP.createNotification(content, NotificationType.INFORMATION).notify(project);
     }
 
 }
