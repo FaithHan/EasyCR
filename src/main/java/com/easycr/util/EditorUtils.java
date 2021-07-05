b10094402f37a00c0cf823b3bd150698a6860d52
feihan <feihan@thoughtworks.com>
2021-07-05 18:04:59 +0800

    code demo indent 4 space

--- src/main/java/com/easycr/util/EditorUtils.java
+++ src/main/java/com/easycr/util/EditorUtils.java
@@ -3,11 +3,11 @@ package com.easycr.util;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class EditorUtils {
 
@@ -26,18 +26,13 @@ public class EditorUtils {
         int lineStartOffset = editor.getDocument().getLineStartOffset(startNotEmptyLineNumber - 1);
         int lineEndOffset = editor.getDocument().getLineEndOffset(startNotEmptyLineNumber - 1);
         String lineContent = editor.getDocument().getText(new TextRange(lineStartOffset, lineEndOffset));
-        if (lines.size() == 1) {
-            return "    " + lines.get(0).trim();
-        } else {
+        if (lines.size() != 1) {
             String firstLine = lines.get(0);
             int i = lineContent.indexOf(firstLine);
-            StringBuilder sb = new StringBuilder();
-            IntStream.range(0, i).forEach(value -> sb.append(" "));
-            sb.append(firstLine);
-            lines.set(0, sb.toString());
-            return String.join("\n", lines);
+            lines.set(0, StringUtils.SPACE.repeat(i) + firstLine);
         }
-
+        lines = IndentFourSpace(lines);
+        return String.join("\n", lines);
     }
 
     public static int getColumn(Editor editor) {
@@ -54,5 +49,42 @@ public class EditorUtils {
         int beginEmptyLineCount = (int) Arrays.stream(Objects.requireNonNull(editor.getSelectionModel().getSelectedText()).split(SPLIT_PATTERN))
                 .takeWhile(line -> line.trim().equals(StringUtils.EMPTY)).count();
         return editor.offsetToLogicalPosition(editor.getSelectionModel().getSelectionStart()).line + 1 + beginEmptyLineCount;
+    }
+
+    private static List<String> IndentFourSpace(List<String> lines) {
+        Pattern pattern = Pattern.compile("(\\s*)(.*)");
+        int indentCount = lines.stream()
+                .filter(line -> !line.trim().isEmpty())
+                .map(line -> {
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        return matcher.group(1);
+                    } else {
+                        return "";
+                    }
+                }).map(String::length)
+                .min(Comparator.comparing(Function.identity()))
+                .map(minSpace -> minSpace - 4)
+                .orElse(0);
+
+        if (indentCount == 0) {
+            return new ArrayList<>(lines);
+        } else {
+            return lines.stream().map(line -> {
+                if (line.trim().isEmpty()) {
+                    return line;
+                }
+                Matcher matcher = pattern.matcher(line);
+                int spaceCount;
+                if (matcher.find()) {
+                    spaceCount = matcher.group(1).length();
+                } else {
+                    spaceCount = 0;
+                }
+                spaceCount = spaceCount - indentCount;
+                return StringUtils.SPACE.repeat(spaceCount) + matcher.group(2);
+            }).collect(Collectors.toList());
+        }
+
     }
 }
--- src/main/java/com/easycr/util/StringUtils.java
+++ src/main/java/com/easycr/util/StringUtils.java
@@ -3,6 +3,7 @@ package com.easycr.util;
 public abstract class StringUtils {
 
     public static final String EMPTY  = "";
+    public static final String SPACE = " ";
 
     public static boolean isEmpty(String string) {
         return string == null || string.isEmpty();
